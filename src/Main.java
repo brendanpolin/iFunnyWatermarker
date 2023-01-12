0abbfd062f0b90a8253bc817a25ec10618b4968d
brendanpolin <brendan@polin.net>
2023-01-12 11:03:49 -0500

    Half way there, but it's broken though the idea of the program should be beginning to shine through

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import javax.swing.text.html.ImageView;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("fileName");
+        File mainDirectory = new File("C:\\");
+        for (String fileName : getAllImages(mainDirectory, true))
+        {
+            System.out.println(fileName);
+        }
+    }
+    public static ArrayList<String> getAllImages(File directory, boolean descendIntoSubDirectories) throws IOException {
+        ArrayList<String> resultList = new ArrayList<String>(256);
+        File[] f = directory.listFiles();
+        for (File file : f) {
+            if (file != null && file.getName().toLowerCase().endsWith(".jpg") && !file.getName().startsWith("tn_")) {
+                resultList.add(file.getCanonicalPath());
+            }
+            if (descendIntoSubDirectories && file.isDirectory()) {
+                ArrayList<String> tmp = getAllImages(file, true);
+                if (tmp != null) {
+                    resultList.addAll(tmp);
+                }
+            }
+        }
+        if (resultList.size() > 0)
+            return resultList;
+        else
+            return null;
     }
 }
